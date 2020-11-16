a38ad26feedbecd8e49d15efa36c1ebc1c8e788f
Alexander <kobzon3000bmx@mail.ru>
2020-11-16 22:26:28 +0400

    update gitignore

--- src/js/createCityByName.js
+++ src/js/createCityByName.js
@@ -65,7 +65,7 @@ const createCityByName = () => {
         if (searchInput.value === '') {
             return;
         } 
-        postCity(searchInput.value);
+        // postCity(searchInput.value);
         // searchInput.value = '' 
         // container.append(list);
         // currentCityContainer.append(container);
